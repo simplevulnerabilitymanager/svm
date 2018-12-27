5974c5d2669cad892dd56be42e866a8a4c9ee426
SimpleVulnerabilityManager <contacto@simplevulnerabilitymanager.com>
2018-12-26 22:07:48 -0300

    Archivos que ya no se usan mas

--- install_upgrade_tools_external_windows.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-@title=[Instalando/Actualizando Tools Externas Windows]
-"%~dp0curl.exe" -o psexec.exe https://live.sysinternals.com/psexec.exe
-"%~dp0curl.exe" -o plink.exe https://the.earth.li/~sgtatham/putty/latest/x86/plink.exe
-
-pause
