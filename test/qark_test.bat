6b5110705bccf395f311acdbd26b757509c77f0b
Simple Vulnerability Manager <contacto@simplevulnerabilitymanager.com>
2019-03-30 00:54:24 -0300

    Update qark_test.bat

--- test/qark_test.bat
+++ test/qark_test.bat
@@ -1,11 +1,7 @@
 
 setlocal
 
-cd c:\svm_test\
-mkdir Reportes
-mkdir Reportes\test\
-
-curl -o "C:\svm_test\Reportes\test\MaterialLoginExample.apk" "https://raw.githubusercontent.com/katalon-studio-samples/Material-Login-App-Test/master/App%20Files/MaterialLoginExample.apk"
+c:\svm_test\curl -o "C:\svm_test\Reportes\test\MaterialLoginExample.apk" "https://raw.githubusercontent.com/katalon-studio-samples/Material-Login-App-Test/master/App%20Files/MaterialLoginExample.apk"
 
 set DirApp="/root/qark/"
 set PathAPK="C:\svm_test\Reportes\test\MaterialLoginExample.apk"
@@ -16,5 +12,7 @@ set Server=192.168.139.153
 set Username=root
 set Password=toor
 
-copy "%WORKSPACE%\qark.bat" c:\svm_test\
+mkdir Reportes
+mkdir Reportes\test\
+copy "%WORKSPACE%\test\qark.bat" c:\svm_test\
 call qark.bat %DirApp% %PathAPK% %FileApk% %Timestamp% %Documentacion% %Server% %Username% %Password%
