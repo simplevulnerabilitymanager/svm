0e7c9d0e02a43e4fea0aaadec2370644cf7a7b3b
Simple Vulnerability Manager <contacto@simplevulnerabilitymanager.com>
2019-03-29 23:58:18 -0300

    jenkins test

--- test/recon-ng_test.bat
+++ test/recon-ng_test.bat
@@ -1,12 +1,15 @@
 setlocal
 set Proyecto="test"
 set Domains=www.example.com
-set Documentacion="c:\svm\Reportes\test\"
+set Documentacion="c:\svm_test\Reportes\test\"
 set Timestamp=09_01_2019-19_21_20
 set Server=192.168.139.153
 set Username=root
 set Password=toor
 
-copy recon_ng_remote.bat c:\svm_test\
+copy %WORKSPACE%\recon_ng_remote.bat c:\svm_test\
 cd c:\svm_test\
+mkdir Reportes
+mkdir Reportes\test\
+echo y | plink.exe -ssh -P 22 -l %Username% -pw %Password% -C %Server% 'ls'
 call recon_ng_remote.bat %Proyecto% %Domains% %Documentacion% %Timestamp% %Server% %Username% %Password%
