49c023e98629718fc9b400775c90dfd7e6b18c17
Simple Vulnerability Manager <contacto@simplevulnerabilitymanager.com>
2019-03-30 00:22:06 -0300

    Test nmap
    Test para probar con jenkins

---
+++ test/nmap_scan_test.bat
@@ -0,0 +1,14 @@
+setlocal
+set Proyecto="test"
+set IP="127.0.0.1"
+set Timestamp=30_03_2019-00_11_56
+set Documentacion="c:\svm_test\Reportes\test\"
+set AppExe="c:\Program Files (x86)\Nmap\nmap.exe"
+
+
+
+copy %WORKSPACE%\nmap_scan.bat c:\svm_test\
+cd c:\svm_test\
+mkdir Reportes
+mkdir Reportes\test\
+call nmap_scan.bat %Proyecto% %IP% %Timestamp% %Documentacion% %AppExe%
