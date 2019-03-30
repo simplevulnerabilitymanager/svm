af6da7b858a50412e4000a4767659ae9ed9a73fc
Simple Vulnerability Manager <contacto@simplevulnerabilitymanager.com>
2019-03-30 01:15:02 -0300

    Update enjarify_remote_test.bat

--- test/enjarify_remote_test.bat
+++ test/enjarify_remote_test.bat
@@ -1,7 +1,7 @@
 
 c:\svm_test\curl -o "C:\svm_test\Reportes\test\MaterialLoginExample.apk" "https://raw.githubusercontent.com/katalon-studio-samples/Material-Login-App-Test/master/App%20Files/MaterialLoginExample.apk"
 
-set DirApp="/root/qark/"
+set DirApp="/root/enjarify/"
 set PathAPK="C:\svm_test\Reportes\test\MaterialLoginExample.apk"
 set FileApk="MaterialLoginExample.apk"
 set Timestamp=30_03_2019-00_40_38
